304cafde46e39a25e56896215153cdabce8f3486
Arkru <35932023+Arkru1204@users.noreply.github.com>
2025-09-08 23:18:07 +0900

    [Chore] Add CableComponent in build file

--- Source/FROGECT/FROGECT.Build.cs
+++ Source/FROGECT/FROGECT.Build.cs
@@ -8,7 +8,7 @@ public class FROGECT : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "CableComponent", "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
